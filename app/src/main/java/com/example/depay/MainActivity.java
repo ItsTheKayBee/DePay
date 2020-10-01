89aa4936b403144ca17d3a6acfcdae0a5c7000d4
Kunal <kunal.bohra@somaiya.edu>
2020-10-02 02:38:51 +0530

    Added bottom navigation bar

--- app/src/main/java/com/example/depay/MainActivity.java
+++ app/src/main/java/com/example/depay/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.depay;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     TextView registerClick;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent goToRegister = new Intent(MainActivity.this, Register.class);
                 startActivity(goToRegister);
+            }
+        });
+
+        login = findViewById(R.id.login_button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToHome = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(goToHome);
             }
         });
     }
