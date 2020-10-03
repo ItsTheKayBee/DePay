5caf2a38be6d9729531b343fb758dc1febb1239c
Kunal <kunal.bohra@somaiya.edu>
2020-10-04 01:18:12 +0530

    Added addMoney activity

--- app/src/main/java/com/example/depay/AddMoney.java
+++ app/src/main/java/com/example/depay/AddMoney.java
@@ -1,6 +1,8 @@
 package com.example.depay;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class AddMoney extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_money);
+        Spinner spinner = findViewById(R.id.payment_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.payment_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/depay/WalletFragment.java
+++ app/src/main/java/com/example/depay/WalletFragment.java
@@ -1,9 +1,11 @@
 package com.example.depay;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,7 +16,16 @@ public class WalletFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wallet, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_wallet, container, false);
+        Button addMoneyButton = view.findViewById(R.id.add_bitcoin_button);
+        addMoneyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToAddMoney = new Intent(getActivity(), AddMoney.class);
+                startActivity(goToAddMoney);
+            }
+        });
+        return view;
     }
 }
